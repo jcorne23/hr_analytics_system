cc14bdb480c57393a6838016e2fc5443198fbfe7
Julio Cornejo <jcorne23@illinois.edu>
2025-10-26 00:49:33 -0500

    dash 1 data clean

---
+++ etl/etl_hr_data.py
@@ -0,0 +1,28 @@
+import pandas as pd 
+df = pd.read_csv('data/raw/hR.csv')
+
+# Assertation for unique employee_id 
+assert df['EmployeeNumber'].is_unique, "Duplicate employee IDs found!"
+
+# Preview 
+print("Shape:", df.shape)
+print(df.head())
+
+# Department cleaning 
+missing_dept = df[df['Department'].isna() | (df['Department'].str.strip() == "")]
+
+# Gender cleaning 
+df['Gender'] = df['Gender'].str.lower().map({
+'m':'Male',
+'male': 'Male',
+'female': 'Female',
+'f' : "Female"
+})
+
+#Salary Cleaning 
+df["MonthlyIncome"] = df["MonthlyIncome"].replace('[\$,]', '', regex=True) 
+df["MonthlyIncome"] = df['MonthlyIncome'].fillna(
+    df.groupby('Department')["MonthlyIncome"].transform('mean'))
+df.to_csv("data/clean/clean.csv", index=False)
+
+
